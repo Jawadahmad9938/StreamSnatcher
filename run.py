9f8e4b8febfc0822cb2f1e9b4f40ec453360998f
jawad <jawadahmad9938@gmail.com>
2025-09-15 16:54:51 +0500

    deployement setup

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from waitress import serve
 from main import app
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
-serve(app, host="127.0.0.1", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway sets PORT env
+    serve(app, host="0.0.0.0", port=port)
