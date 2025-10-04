1c05e9a56f2768fe3c17e0ea26411c16e0053637
jawad <jawadahmad9938@gmail.com>
2025-10-04 17:52:45 +0500

    production setup deploy

--- run.py
+++ run.py
@@ -3,15 +3,9 @@ import logging
 from waitress import serve
 from main import app
 
-# ---------------------------
-# Logging setup
-# ---------------------------
 logging.basicConfig(level=logging.INFO)
 logging.info("Starting application...")
 
-# ---------------------------
-# Run server
-# ---------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
